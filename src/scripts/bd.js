6cfc6eee804aef93be7435912f00a11f8f4edd56
Felipe Moreira <felipemoreira.97@hotmail.com>
2020-09-25 17:43:21 -0300

    Estrutura dos cards finalizada

---
+++ src/scripts/bd.js
@@ -0,0 +1,32 @@
+let data = [
+  {
+    category: "Mobile",
+    year: "2018",
+    url_image: "../images/cards/1.png",
+    key: 1,
+  },
+  {
+    category: "Web",
+    year: "2019",
+    url_image: "../images/cards/2.png",
+    key: 2,
+  },
+  {
+    category: "Mobile",
+    year: "2020",
+    url_image: "../images/cards/3.png",
+    key: 3,
+  },
+  {
+    category: "Web",
+    year: "2017",
+    url_image: "../images/cards/4.png",
+    key: 4,
+  },
+  {
+    category: "Mobile",
+    year: "2020",
+    url_image: "../images/cards/4.png",
+    key: 5,
+  },
+];
