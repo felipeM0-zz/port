eb538f8f7ffc4cf45e26dfbfd2fa882aab612b7f
Felipe Moreira <felipemoreira.97@hotmail.com>
2020-09-25 21:39:45 -0300

    Incluido a seleção das opções dos cards

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,71 +1,50 @@
-function addAllCards() {
-  let elem = document.getElementById("row-cards");
-  let xhr = new XMLHttpRequest();
-  xhr.open(
-    "GET",
-    "https://raw.githubusercontent.com/felipeM0/port/master/src/scripts/bd.json"
-  );
+function addAllCards(type, active) {
+  if (!active) {
+    let elem = document.getElementById("row-cards");
+    elem.innerHTML = "";
+    let xhr = new XMLHttpRequest();
+    xhr.open(
+      "GET",
+      "https://raw.githubusercontent.com/felipeM0/port/master/src/scripts/bd.json"
+    );
 
-  beforeSend();
+    beforeSend();
+    xhr.send();
 
-  xhr.send();
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState == 4) {
+        if (xhr.status == 200) {
+          elem.innerHTML = "";
+          let itens = JSON.parse(xhr.responseText);
+          itens.forEach((e) => {
+            if (e.category == type || type == "Tudo") {
+              let card = `<div class="card">
+                                    <div class="card-image">
+                                        <img class="b-lazy" data-src="${e.url_image}" src="https://www.dkflooring.pt/assets/images/loading.gif" alt="${e.title}"/>
+                                    </div>
+                                    <div class="card-content">
+                                        <p>${e.category}</p>
+                                        <div>
+                                            <p>${e.title}</p>
+                                            <p>${e.year}</p>
+                                        </div>
+                                    </div>
+                                </div>`;
 
-  xhr.onreadystatechange = () => {
-    if (xhr.readyState == 4) {
-      if (xhr.status == 200) {
-        elem.innerHTML = "";
-        var itens = JSON.parse(xhr.responseText);
-        itens.forEach((e) => {
-          var card = `<div class="card">
-                            <div class="card-image">
-                                <img src="${e.url_image}"/>
-                            </div>
-                            <div class="card-content">
-                                <p>${e.category}</p>
-                                <div>
-                                    <p>${e.title}</p>
-                                    <p>${e.year}</p>
-                                </div>
-                            </div>
-                        </div>`;
-
-          elem.innerHTML += card;
-        });
+              elem.innerHTML += card;
+            }
+          });
+          let bLazy = new Blazy({ offset: -125 });
+        }
       }
-    }
-  };
+    };
+  }
 }
 
 function beforeSend() {
-  var itens = `<div class="ph-item card">
-                    <div class="ph-col-4-mod">
-                        <img src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" class="ph-picture responsive-img"/>
-                    </div>
-                    <div class="ph-col-8-mod">
-                        <div class="ph-row">
-                            <div class="ph-col-6"></div>
-                        </div>
-                        <div class="ph-row">
-                            <div class="ph-col-8 big"></div>
-                            <div class="ph-col-2 big"></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="ph-item card">
-                    <div class="ph-col-4-mod">
-                        <img src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" class="ph-picture responsive-img"/>
-                    </div>
-                    <div class="ph-col-8-mod">
-                        <div class="ph-row">
-                            <div class="ph-col-6"></div>
-                        </div>
-                        <div class="ph-row">
-                            <div class="ph-col-8 big"></div>
-                            <div class="ph-col-2 big"></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="ph-item card">
+  let elem = document.getElementById("row-cards");
+
+  let itens = `<div class="ph-item card">
                     <div class="ph-col-4-mod">
                         <img src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" class="ph-picture responsive-img"/>
                     </div>
@@ -108,5 +87,5 @@ function beforeSend() {
                     </div>
                 </div>`;
 
-  document.getElementById("row-cards").innerHTML += itens;
+  elem.innerHTML += itens;
 }
